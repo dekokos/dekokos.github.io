4d5c92801dceb6e2fdae0f44ff0fd39ad477f5bc
DeKo <dekoxingyun@gmail.com>
2017-12-08 00:46:14 +0300

    update

--- new-pc/src/scripts.js
+++ new-pc/src/scripts.js
@@ -1,18 +1,45 @@
-$(function () {
 
-	$('nav a').click(function(event) {
-    event.preventDefault();
-    var href=$(this).attr('href');
-    var target=$(href);
-    var top=target.offset().top;
-    $('html,body').animate({
-      scrollTop: top
-    }, 1000);
-  });
+window.onload = function() {
+  
+  
+    var addCollection = document.querySelectorAll('.add');
+    var clearCounter = document.querySelector('.count-clear');
+  
+    for (var i = 0; i < addCollection.length; i++) {
+      var addCol = addCollection[i];
+      
+  
+      addCol.addEventListener('click', function(e) {
+        e.preventDefault();
+        var numAddCol = +this.previousElementSibling.innerHTML.replace('₴', '').replace(/\s+/g, '');
+        // console.log(parseInt(numAddCol));
+        // console.log(typeof(numAddCol));
+        // console.log(numAddCol);
+        var counter = document.querySelector('.counter');
+        var counterInn = counter.innerHTML;
+        // console.log(+counterInn);
+        counter.innerHTML = +counterInn + numAddCol;
+        // console.log(counter);
+      });
+    }
+  
+    clearCounter.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('.counter').innerHTML = '0';
+    });
+  	// $('nav a').click(function(event) {
+    //   event.preventDefault();
+    //   var href=$(this).attr('href');
+    //   var target=$(href);
+    //   var top=target.offset().top;
+    //   $('html,body').animate({
+    //     scrollTop: top
+    //   }, 1000);
+    // });
+  }
+// $(function () {
 
-  // $('.add').click(function(e) {
-  // 	e.preventDefault();
-  	
-  // });
 
-})
+
+// });
+
