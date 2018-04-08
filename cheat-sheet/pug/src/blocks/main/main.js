0d33f1a379b142dfb65c77c8ca8d944f9118052d
DeKo <dekoxingyun@gmail.com>
2018-04-09 00:22:51 +0300

    update cheat-sheet

--- cheat-sheet/pug/src/blocks/main/main.js
+++ cheat-sheet/pug/src/blocks/main/main.js
@@ -4,11 +4,41 @@ $(function () {
         e.stopPropagation();
         // $(this).nextAll("pre").hide(200);
         $(this).find(".main-content").slideToggle(200);
+        $(this).find(".copy-btn").slideToggle(200);
         $(this).find(".inner-title").find(".main-content").hide();
+        $(this).find(".inner-title").find(".copy-btn").hide();
     });
     $(".inner-title").click(function(e) {
         e.stopPropagation();
        $(this).find(".main-content").slideToggle(200);
+       $(this).find(".copy-btn").slideToggle(200);
     });
+    $(".main-content").click(function(e) {
+        e.stopPropagation();
+    });
+    $(".copy-btn").click(function(e) {
+       e.stopPropagation();
+       let text = $(this).next(".main-content");
+        copyToClipboard(text);
+        text.css({
+            "color": "slategray",
+            "transition": "all .3s linear"
+        });
+        setTimeout(function() {
+            text.css({
+                "color": "#000",
+                "transition": "none"
+            });
+        }, 1000);
+    });
+
+    // копирование текста в буфер обмена
+    function copyToClipboard(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
 
 });
